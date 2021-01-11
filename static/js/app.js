4d9b610eb0f00dadd4486bbc058572e3aff587ed
MuratCKOC <muratcagrikoc@gmail.com>
2021-01-11 16:13:40 -0500

    retrieve date entry

--- static/js/app.js
+++ static/js/app.js
@@ -2,16 +2,19 @@
 var tableData = data;
 
 // Get a reference to the table body
-var tbody = d3.select(".ufo-table");
+var tbody = d3.select("tbody");
 var form = d3.select("#form")
 var filterButton = d3.select("#filter-btn")
 // Console.log the weather data from data.js
 console.log(data);
-var inputDate = d3.select("#datetime").property("value");
-console.log(inputDate);
 
 // Create event handlers
-filterButton.on("click",fillTable());
+//filterButton.on("click",fillTable(function(){
+//    var inp = d3.select(".form-control").property("value");
+//    return inp;
+//}));
+
+filterButton.on("click",fillTable);
 form.on("submit",fillTable);
 
 // YOUR CODE HERE!
@@ -19,12 +22,16 @@ function fillTable(){
     //Stop refresh
     //d3.event.preventDefault();
     
-    console.log(inputDate);
-// BONUS: Refactor to use Arrow Functions!
+    //var varibe = d3.select("#datetime").property("value");
+    var inputDate = d3.select("#datetime");
+    var inputValue = inputDate.property("value");
+    var tbody = d3.select(".ufo-table");
+    d3.event.target.value;
 
+    //var varibe = d3.select("#datetime").property("value");
+    console.log(varibe);
+    console.log("ASDASDASD");
+    console.log(inputValue);
+// BONUS: Refactor to use Arrow Functions!
 }
 
-
-var inputDate = d3.select("#datetime").text()
-console.log(inputDate)
-d3.select("#filter-btn").on("click", fillTable())
