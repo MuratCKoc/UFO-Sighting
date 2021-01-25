2ae91dabdecaa0d1bb51782658f08f2223d01c8e
MuratCKOC <muratcagrikoc@gmail.com>
2021-01-25 00:13:15 -0500

    options ui completed

--- static/js/app.js
+++ static/js/app.js
@@ -6,23 +6,28 @@ var tbody = d3.select("tbody");
 var form = d3.select("#form")
 var filterButton = d3.select("#filter-btn")
 
-var filterZone = []
-
 var cityOptions = []
 var stateOptions = []
 var countryOptions = []
 var shapeOptions = []
-
+var datetimeOptions = []
 //Get distinct values from data and store them into dropdown option filters
 
 //Get distinct values from data
     tableData.forEach(function(rec) {
+        if (datetimeOptions.includes(rec.datetime)===false){datetimeOptions.push(rec.datetime);}
         if (cityOptions.includes(rec.city)===false){cityOptions.push(rec.city);}
         if (stateOptions.includes(rec.state)===false){stateOptions.push(rec.state);}
         if (countryOptions.includes(rec.country)===false){countryOptions.push(rec.country);}
         if (shapeOptions.includes(rec.shape)===false){shapeOptions.push(rec.shape);}
     });
 
+var filterZone = {
+        "City":cityOptions,
+        "State":stateOptions,
+        "Country":countryOptions,
+        "Shape":shapeOptions};
+console.log(filterZone);
 
 // Console.log the weather data from data.js
 console.log(data);
