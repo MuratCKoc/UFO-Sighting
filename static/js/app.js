7fc1a2ec3cb3b84040d3b4c2eb18b7382906cb61
MuratCKOC <muratcagrikoc@gmail.com>
2021-01-24 19:22:46 -0500

    declutter UFO-Level-1

--- static/js/app.js
+++ static/js/app.js
@@ -8,21 +8,6 @@ var filterButton = d3.select("#filter-btn")
 // Console.log the weather data from data.js
 console.log(data);
 
-//var varibe = d3.select("#datetime").property("value");
-//var inputDate = d3.select("#datetime");
-//var inputValue = inputDate.property("value");
-
-//Prefill the table
-//tableData.forEach(function(rec){
-//    var row = tbody.append("tr");
-//    Object.entries(rec).forEach(([key,value]) => row.append("td").text(value));
-//});
-
-// Create event handlers
-//filterButton.on("click",fillTable(function(){
-//    var inp = d3.select(".form-control").property("value");
-//    return inp;
-//}));
 
 filterButton.on("click",fillTable);
 form.on("submit",fillTable);
@@ -37,20 +22,14 @@ function fillTable(){
     var tbody = d3.select("tbody");
     d3.event.target.value;
 
+    //Filter data according to date
     var filtered = tableData.filter(item => item.datetime === inputValue);
-    console.log(filtered);
-    console.log(inputValue);
     filtered.forEach(function(rec){
     var row = tbody.append("tr");
     Object.entries(rec).forEach(([key,value]) => row.append("td").text(value));
 });
-
-
-  //d3.select("tbody").selectAll("tr").data(filtered).enter().append("tr").html(function(d) {
-  //    return `"<td>${inputValue.datetime}</td><td>${inputValue.city}</td>`
-  //})
     console.log("ASDASDASD");
     console.log(inputValue);
-// BONUS: Refactor to use Arrow Functions!
+
 }
 
